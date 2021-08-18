9cdf573c1f7ee2eec2c9c9c5da1ee7559d9e22c3
Peaceosareme <87088482+Peaceosareme@users.noreply.github.com>
2021-08-18 14:16:51 +0100

    Add files via upload

---
+++ guessing_games.py
@@ -0,0 +1,302 @@
+username= input('Enter your name: ')
+print('welcome ' + username)
+
+
+
+
+def egg():
+    print('you are given riddles and you should guess the word')
+    print('riddle: what has to be broken before you use it')
+    answer= 'egg'
+    ans = input('enter our answer: ')
+    while ans != answer:
+        print('incorrect! try again')
+        ans = input('enter our answer: ')
+    print('correct!')
+egg()
+print("")
+def candle():
+    print("Level 2")
+    print(' riddle: i am tall when i am young, and i am short when i am old.')
+    answer= 'candle'
+    ans = input("enter your answer: ")
+    while ans != answer:
+        print('incorrect! try again')
+        ans = input('enter your answer')
+    print('bravo!')
+candle()
+print('')
+def month():
+    print('Level 3')
+    print('riddle: what month of the year has 28 days')
+    answer = 'all of them'
+    ans = input('enter your answer: ')
+    while ans != answer:
+        print('incorrect! try again')
+        ans = input('enter your answer: ')
+    print("excellent!")
+month()
+print('')
+def sponge():
+    print('Level 4')
+    print('riddle: what is full of holes but holds water')
+    answer= 'sponge'
+    ans = input("enter you answer: ")
+    while ans != answer:
+        print('incorrect! try again')
+        answer = 'sponge'
+        ans = input('enter your answer: ')
+    print("hurray!")
+sponge()
+print('')
+def promise():
+    print ('Level 5')
+    print(' riddle: what can you break, even if you never touch it or pick it up ')
+    answer = 'a promise'
+    ans = input ('enter your answer: ')
+    while ans != answer:
+         print('incorrect! try again')
+         ans = input("enter your answer: ")
+    print('correct!')
+promise()
+print('')
+def towel():
+    print('Level 6')
+    print(' riddle: what gets wet while drying')
+    answer = 'towel'
+    ans = input('enter your answer:')
+    while ans != answer:
+        print('incorrect! try again')
+        ans = input('enter your answer:')
+    print('bravo!')
+towel()
+print('')
+def barber():
+    print('Level 7')
+    print("riddle: i shave everyday, but my beard stays the same. what am i?")
+    answer= 'barber'
+    ans = input('enter your answer:')
+    while ans != answer:
+        print('incorrect! try again')
+        ans = input('enter your answer:')
+    print('correct!')  
+barber()
+print('')
+def age():
+    print('Level 8')
+    print('riddle: what goes up but never comes down')
+    answer = 'age'
+    ans = input ('enter your answer: ')
+    while ans != answer:
+        print('wrong')
+        ans = input ('enter your answer: ')
+    print('correct')
+age()
+def bank():
+    print('level 9')
+    print('riddle: i have branches, but no fruit, trunk or leaves. what am i?')
+    ans= input('enter your answer: ')
+    answer = 'a bank'
+  
+    while ans != answer :
+        print('incorrect! try again')
+        ans = input ('enter your answer: ')
+    print('wonderful!')
+bank()
+print('')
+def david():
+    print('level 10')
+    print("riddle: David's parents has 3 sons: Snap, Crackle, and what's the name of the third son? ")
+    print("hint: name start should start with capital letter")
+    answer = 'David'
+    ans = input('enter your answer: ')
+    while ans != answer:
+        print('incorrect! try again')
+        ans = input ('enter your answer: ')
+    print('correct!')
+david()
+print('')
+def shadow():
+    print('level 11')
+    print("riddle: I follow you everywhere and copy your every move, but you can't touch me or catch me. what am I?")
+    answer= 'shadow'
+    ans = input('enter your answer: ')
+    while ans != answer:
+        print('incorrect! try again')
+        ans = input ('enter your answer: ')
+    print('correct!')
+shadow()
+print('')
+def piano():
+    print('level 12')
+    print(" riddle: what has many keys but can not open a single lock.")
+    answer = 'piano'
+    ans = input('enter your answer: ')
+    while ans != answer:
+        print('incorrect! try again')
+        ans = input ('enter your answer: ')
+    print('correct!')
+piano()
+print('')
+def chalkboard():
+    print('level 13!!')
+    print("riddle: what is back when it's clean and white when it's dirty?")
+    answer= 'chalkboard'
+    ans = input('enter your answer: ')
+    while ans != answer:
+        print('incorrect! try again')
+        ans = input ('enter your answer: ')
+    print('correct!')
+chalkboard()
+print('')
+def elbow():
+    print("level 14!")
+    print("riddle: what can you hold in your left hand but not in your right? ")
+    answer = 'right elbow'
+    ans = input('enter your answer: ')
+    while ans != answer:
+        print('incorrect! try again')
+        ans = input ('enter your answer: ')
+    print('correct!')
+elbow()
+print('')
+def breath():
+    print('level 15!')
+    print("riddle: I'm light as a feather, yet the strongest person can't hold me for five minutes. what am I?")
+    answer = 'breath'
+    ans = input('enter your answer: ')
+    while ans != answer:
+        print('incorrect! try again')
+        ans = input ('enter your answer: ')
+    print('correct!')
+breath()
+print('')
+def yarn():
+    print("level 16")
+    print("riddle: I'm found in socks, scarves and mittens; and often in the paws of playful kittens. what am i?")
+    answer = 'yarn'
+    ans = input('enter your answer: ')
+    while ans != answer:
+        print('incorrect! try again')
+        ans = input ('enter your answer: ')
+    print('correct!')
+yarn()
+print('')
+def window():
+    print("level 17!")
+    print("riddle: what invention lets you look right through a wall?")
+    answer= 'window'
+    ans = input('enter your answer: ')
+    while ans != answer:
+        print('incorrect! try again')
+        ans = input ('enter your answer: ')
+    print("correct!")
+window()
+print('')
+def staircase():
+    print("level 18!")
+    print("riddle: what goes up and down but does not move?")
+    answer= staircase
+    ans = input('enter your answer: ')
+    while ans != answer:
+        print('incorrect! try again')
+        ans = input ('enter your answer: ')
+    print('correct!')
+staircase()
+print("")
+def second_place():
+    print("level 19!")
+    print("riddle: if you're running in a race and you pass the person in second place, what place are you in?")
+    answer = 'second place'
+    ans = input('enter your answer: ')
+    while ans != answer:
+        print('incorrect! try again')
+        ans = input ('enter your answer: ')
+    print('correct!')
+second_place()
+print("")
+def name():
+    print('level 20!')
+    print("riddle: it belongs to you, but other people use it more than you do. what is it?")
+    answer= 'name'
+    ans = input('enter your answer: ')
+    while ans != answer:
+        print('incorrect! try again')
+        ans = input ('enter your answer: ')
+    print('correct!')
+name()
+print('')
+def needle():
+    print("level 21!")
+    print("riddle: what has one eye but can  not see?")
+    answer= ' a needle'
+    ans = input('enter your answer: ')
+    while ans != answer:
+        print('incorrect! try again')
+        ans = input ('enter your answer: ')
+    print('correct!')
+needle()
+print('')
+def clock():
+    print("level 22")
+    print("riddle: what has hands but can't clap?")
+    answer= 'a clock'
+    ans = input('enter your answer: ')
+    while ans != answer:
+        print('incorrect! try again')
+        ans = input ('enter your answer: ')
+    print('correct!')
+clock()
+print('')
+def cold():
+    print("level 23")
+    print("riddle: what can you catch, but can't throw")
+    answer ='a cold'
+    ans = input('enter your answer: ')
+    while ans != answer:
+        print('incorrect! try again')
+        ans = input ('enter your answer: ')
+    print('correct!')
+cold()
+print('')
+def rubber():
+    print("level 24!")
+    print("riddle: what kind of band never plays music?")
+    answer = 'rubber band'
+    ans = input('enter your answer: ')
+    while ans != answer:
+        print('incorrect! try again')
+        ans = input ('enter your answer: ')
+    print('correct!')
+rubber()
+print('')
+def comb():
+    print("level 25!!")
+    print("riddle: what has many teeth but can't bite?")
+    answer= 'comb'
+    ans = input('enter your answer: ')
+    while ans != answer:
+        print('incorrect! try again')
+        ans = input ('enter your answer: ')
+    print('correct!')
+comb()
+print("")
+def book():
+    print("level 26!")
+    print("riddle: what has words, but never speaks?")
+    answer= ' a book'
+    ans = input('enter your answer: ')
+    while ans != answer:
+        print('incorrect! try again')
+        ans = input ('enter your answer: ')
+    print('correct!')
+book()
+print("")
+
+
+
+
+
+
+
+
